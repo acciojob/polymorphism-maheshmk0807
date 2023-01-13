0c71f6da6fc45c234f9d64a7e4fc56dff57a5c4e
Mahesh Khedkar <maheshmk0807@gmail.com>
2023-01-13 23:58:17 +0530

    a

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,17 +8,17 @@ public class Main {
     p.product(3,6,9);
     p.product(2d,5d);
     }
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
 
-class Product{
-    public int product(int x, int y) {
-        return x*y;
-    }
-    public int product(int x, int y, int z) {
-        return x*y*z;
-
-    }
-    public double product(double x, double y) {
-        return x*y;
-    }
-}
